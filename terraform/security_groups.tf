5b2bb8bda0a80ee484bc1ea73446a738698cb2c6
alondudi <alondddahan@gmail.com>
2026-03-10 11:55:00 +0200

    fix(infra): update RDS security groups, fix DB routing, and stabilize K8s deployment

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "rds" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.eks_nodes.id] 
+    cidr_blocks = [module.vpc.vpc_cidr_block]
   }
 
   tags = {
@@ -43,7 +43,7 @@ resource "aws_security_group" "redis" {
     from_port       = 6379
     to_port         = 6379
     protocol        = "tcp"
-    security_groups = [aws_security_group.eks_nodes.id]
+    cidr_blocks = [module.vpc.vpc_cidr_block]
   }
 
   tags = {
